50031f22f4153a1a9e2c5301bc78d933ed0ed4b4
flavorblasted <61622436+flavorblasted@users.noreply.github.com>
2020-02-29 07:46:28 -0500

    Update wifiChecker.py

--- wifiChecker.py
+++ wifiChecker.py
@@ -11,12 +11,10 @@ INTERFACE = "interface"
 # How frequently to check if wifi is down. In seconds
 FREQUENCY = 4
 
-# Enables wifi
 def enable():
     connectStr = "netsh wlan connect ssid=%s name=%s interface=%s" % (SSID,NAME,INTERFACE)
     os.system(connectStr)
 
-# Disables wifi
 def disable():
     os.system("netsh wlan disconnect")
 
@@ -24,7 +22,7 @@ def disable():
 def check():
     return os.system("ping 8.8.8.8") == 0
 
-# If wifi goes down, disable then re-enable
+# If wifi goes down, diconnect then reconnect
 while True:
     if not check():
         disable()
