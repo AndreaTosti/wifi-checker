45a1e709c35ec9abac6058645483443d7be74e38
Andrea Tosti <39533805+AndreaTosti@users.noreply.github.com>
2020-07-14 00:59:31 +0200

    Update wifiChecker.py

--- wifiChecker.py
+++ wifiChecker.py
@@ -1,3 +1,8 @@
+try:
+    import httplib
+except:
+    import http.client as httplib
+
 import os
 from time import sleep
 
@@ -11,20 +16,33 @@ INTERFACE = "interface"
 # How frequently to check if wifi is down. In seconds
 FREQUENCY = 4
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 def enable():
+    os.system("netsh interface set interface Wi-Fi ENABLED")
     connectStr = "netsh wlan connect ssid=%s name=%s interface=%s" % (SSID,NAME,INTERFACE)
+    print(connectStr)
     os.system(connectStr)
 
 def disable():
-    os.system("netsh wlan disconnect")
+    os.system("netsh interface set interface Wi-Fi DISABLED")
 
-# Returns True if ping is successful
-def check():
-    return os.system("ping 8.8.8.8") == 0
-
+def have_internet():
+    conn = httplib.HTTPConnection("www.google.com", timeout=5)
+    try:
+        conn.request("HEAD", "/")
+        conn.close()
+        return True
+    except:
+        conn.close()
+        return False
+    
 # If wifi goes down, diconnect then reconnect
 while True:
-    if not check():
+    cls()
+    print("Connessione OK" if have_internet() else "Nessuna connessione verso %s, \nSe vuoi scegliere una rete diversa, chiudi questa finestra\n" % (NAME))
+    if not have_internet():
         disable()
         sleep(.5)
         enable()
